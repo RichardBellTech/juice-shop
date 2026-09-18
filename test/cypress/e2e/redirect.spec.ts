cd796facc609366d541f274c3e4a13866f07fd4b
RichardBellTech <329561135+RichardBellTech@users.noreply.github.com>
2026-09-17 18:34:06 -0700

    Verifying redirect response without visiting external website
    Updated redirect test to verify server response without loading external site.

--- test/cypress/e2e/redirect.spec.ts
+++ test/cypress/e2e/redirect.spec.ts
@@ -10,13 +10,22 @@ describe('/redirect', () => {
 
   describe('challenge "redirectChallenge"', () => {
     it('should redirect to target URL if allowlisted URL is contained in it as parameter', () => {
-      cy.visit(
-        '/redirect?to=https://owasp.org?trickIndexOf=https://github.com/juice-shop/juice-shop',
-        {
-          failOnStatusCode: false
-        }
-      )
-      cy.url().should('match', /https:\/\/owasp\.org/)
+      const destination =
+        'https://owasp.org?trickIndexOf=https://github.com/juice-shop/juice-shop'
+
+      cy.visit('/')
+
+      // Verify the server response without loading the external website.
+      cy.request({
+        method: 'GET',
+        url: '/redirect',
+        qs: { to: destination },
+        followRedirect: false
+      }).then((response) => {
+        expect(response.status).to.eq(302)
+        expect(response.headers.location).to.eq(destination)
+      })
+
       cy.expectChallengeSolved({ challenge: 'Allowlist Bypass' })
     })
   })
